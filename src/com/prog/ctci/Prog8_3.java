bc4b6943af0e2aeced8010eaa33b94cdd7527be7
ptemkar <weltmeister.7@gmail.com>
2013-07-20 11:08:37 -0700

    CTCI 8.3 Write a method that returns all subsets of a set.

--- src/com/prog/ctci/Prog8_3.java
+++ src/com/prog/ctci/Prog8_3.java
@@ -1,24 +1,54 @@
 package com.prog.ctci;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  * Write a method that returns all subsets of a set.
+ * 
+ * refer to the algorithm from here : http://911programming.wordpress.com/2010/06/07/java-extracting-all-subsets-of-a-set-using-arraylist/
+ * 
  * 
  * @author ptemkar
  * @date 07/19/13
  */
 public class Prog8_3 {
-	public void print_all_subsets_recursive(char[] arr, int index){
-		for(int i=index; i<arr.length; i++){
-			
-			print_all_subsets_recursive(arr, i+1);
-			
-			System.out.print(arr[i] + " ");
-			if(i == arr.length-1){
-				System.out.println();
-			}
+	
+	List<String> allSubsets = new ArrayList<String>();
+	
+	public List<String> print_all_subsets_recursive(char[] arr, int index){
+		
+		if(index == arr.length-1){
+			allSubsets.add("");
+			allSubsets.add(String.valueOf(arr[index]));
+			return allSubsets;
 		}
+		
+		unionOf(arr[index], print_all_subsets_recursive(arr, index+1));
+		
+		return allSubsets;
 		
 		
 	}
+	
+	public void displaySubsets(){
+		for(String subset : allSubsets){
+			System.out.println(subset);
+		}
+	}
+	
+	public void unionOf(char newChar, List<String> allSubsets){
+		List<String> newSubsets = new ArrayList<String>();
+		
+		for(String subset : allSubsets){
+			newSubsets.add(subset + newChar);
+		}
+		
+		for(String newSubset : newSubsets){
+			allSubsets.add(newSubset);
+		}
+		
+	}
+	
 }
