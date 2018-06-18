5e4e413ca84b2d27a766086c740954990a44ec95
PT <prateektemkar@PRT-mbk.local>
2018-06-17 23:09:28 -0700

    chp 6
    chp 6

--- python/py1.py
+++ python/py1.py
@@ -1,6 +1,12 @@
 #chapter 1
 name = "pt"
+print(name)
+
 speed = 40
 speed = speed + 30
-print(name)
-print(speed)
+print("speed = " + `speed`)
+
+#chapter 6
+num_ch6 = 2
+num_ch6 += 3
+print("num_ch6 = " + `num_ch6`)
