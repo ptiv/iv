c34917078a1fa2678ea04de2f611fb77dc146b32
PT <prateektemkar@PRT-mbk.local>
2018-06-19 23:36:21 -0700

    ch_8
    ch_8

--- python/py1.py
+++ python/py1.py
@@ -1,12 +1,19 @@
-#chapter 1
-name = "pt"
-print(name)
 
-speed = 40
-speed = speed + 30
-print("speed = " + `speed`)
+#chapter 8
+str_ch8_1 = "hello, "
+str_ch8_2 = "world!"
+print(str_ch8_1 + str_ch8_2)
 
 #chapter 6
 num_ch6 = 2
 num_ch6 += 3
 print("num_ch6 = " + `num_ch6`)
+
+
+speed = 40
+speed = speed + 30
+print("speed = " + `speed`)
+
+#chapter 1
+name = "pt"
+print(name)
