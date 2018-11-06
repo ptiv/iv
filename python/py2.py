8758be9c8336003244864093d53b4e597884cc30
PT <prateektemkar@PRT-mbk.local>
2018-11-06 07:46:12 -0800

    Chapter 21: For Loops
    Chapter 21: For Loops

--- python/py2.py
+++ python/py2.py
@@ -1,1 +1,18 @@
 #py2.py - Chapters 21 to 40
+
+#Chapter 21
+print("Chapter 21: For Loops")
+clean_cities = ["Cheyenne", "Santa Fe", "Tucson"]
+city_to_check_1 = "Santa Fe"
+city_to_check_2 = "New York"
+
+for clean_city in clean_cities:
+	if city_to_check_1 == clean_city:
+		print(city_to_check_1 + " is a clean city")
+
+
+for clean_city in clean_cities:
+	if city_to_check_2 == clean_city:
+		print(city_to_check_2 + " is a clean city")
+
+print("---")
